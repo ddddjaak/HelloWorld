6f74fbc80ac180f555c0e80c965a332f32cb7653
yufan <1239962546@qq.com>
2022-01-16 19:54:08 +0800

    111

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 package com.yufan.demo01;
 
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("HelloWorld");
     }
 }
